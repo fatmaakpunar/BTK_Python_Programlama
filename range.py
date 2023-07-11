51301d989020f037e633d3f0141eec45191f62b7
fatma <123814832+fatmaakpunar@users.noreply.github.com>
2023-07-11 07:35:27 +0300

    Add files via upload

---
+++ range.py
@@ -0,0 +1,14 @@
+#range() => 
+
+#1
+# for item in range(10):
+#     print(item)
+
+#2
+# for item in range(2,10):
+#     print(item)
+
+#3
+for item in range(50,100,20):#50 den 100 e kadar 20 şer 20 şer
+    print(item)
+print(list(range(50,100,20)))#liste içine alır
